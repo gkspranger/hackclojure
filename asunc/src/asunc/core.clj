381e958a4e212b98cd426834fc1dea1f9f45e726
gkspranger <greg@spranger.us>
2020-06-06 18:55:36 -0400

    this is a lot of fun

--- asunc/src/asunc/core.clj
+++ asunc/src/asunc/core.clj
@@ -18,9 +18,60 @@
      (Thread/sleep 3000)
      (a/>! mychan2 "oper 2"))))
 
+(defn do2 []
+  (let [mychan1 (a/chan)]
+    (a/go-loop
+     [cntr 0]
+     (let [op1 (a/<! mychan1)]
+       (if op1
+         (do
+           (println (str op1 " hello "))
+           (Thread/sleep 333)
+           (recur (inc cntr)))
+         (println "all done"))))
+    
+    (a/go-loop
+     [cntr 0]
+     (a/>! mychan1 (str "op1-" cntr))
+     (Thread/sleep 333)
+     (recur (inc cntr)))
+    
+    (a/go
+     (Thread/sleep 11111)
+     (a/close! mychan1)
+     (println "closed channel"))))
+
+(defn do3 []
+  (let [mychan1 (a/chan 10)]
+    (a/go-loop
+     []
+      (let [op1 (a/<! mychan1)]
+        (if op1
+          (do
+            (println (str "got part " op1))
+            (Thread/sleep 333)
+            (recur))
+          (println "all done getting parts"))))
+
+    (a/go-loop
+     [cntr 0]
+     (if (a/>! mychan1 (str "part-" cntr))
+       (do
+         (println (str "added part " cntr))
+         (Thread/sleep 222)
+         (recur (inc cntr)))
+       (println "all done adding parts")))
+    
+    (a/go
+     (Thread/sleep 11111)
+     (a/close! mychan1)
+     (println "closed channel"))))
+
 (comment
   
   (println "hello world")
   (do1)
+  (do2)
+  (do3)
   
   )
